a8b0c26b46bad9a4c5184ed69d13cc03c56755c7
Ze_skoly <budine.03@spst.eu>
2021-05-07 10:42:47 +0200

    Druhe tlacitko

--- ProjektSkola/Form1.cs
+++ ProjektSkola/Form1.cs
@@ -21,5 +21,10 @@ namespace ProjektSkola
         {
             MessageBox.Show("ahojky");
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Nazdar");
+        }
     }
 }
